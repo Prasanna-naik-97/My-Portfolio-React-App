1c69b0d644fe5838418382d4f8a9abedf58d64cb
Prasanna_Naik_dws <prasanna.naik@merkle.com>
2023-08-07 17:30:55 +0530

    Work experience component changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Header } from "./components/header/Header";
 import Description from "./components/description/Description";
+import Experience from "./components/experience/Experience";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header/>
       <div className="main">
         <Description />
+        <Experience />
       </div>
     </div>
   );
